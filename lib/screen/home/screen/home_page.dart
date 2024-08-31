145cb1fa19f157b4be8d4ec8cc619d247422c5d6
Md Apon Ahmed <mdaponahmed412@gmail.com>
2024-08-31 13:28:53 +0600

    in app update add

--- lib/screen/home/screen/home_page.dart
+++ lib/screen/home/screen/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:toolify/app/cube_timer/cube_timer.dart';
@@ -9,9 +11,80 @@ import '../../../app/countdown_timer/countdown_timer.dart';
 import '../../../app/plant_identify/plant_identify.dart';
 import '../../../app/qr_generate/qr_generate.dart';
 import '../widgets/home_page_card.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    checkForUpdate();
+  }
+
+  Future<void> checkForUpdate() async {
+    try {
+      final response = await http.get(
+          Uri.parse('https://apon06.github.io/bookify_api/app_update.json'));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        String latestVersion = data['latest_version'];
+        String updateMessage = data['update_message'];
+
+        PackageInfo packageInfo = await PackageInfo.fromPlatform();
+        String currentVersion = packageInfo.version;
+
+        if (latestVersion != currentVersion) {
+          showUpdateDialog(updateMessage);
+        }
+      } else {
+        //
+      }
+    } catch (e) {
+      //
+    }
+  }
+
+  void showUpdateDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Update Available'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Later'),
+          ),
+          TextButton(
+            onPressed: () async {
+              const String appUpdateUrl =
+                  'https://github.com/apon06/toolify/releases';
+
+              final Uri url = Uri.parse(appUpdateUrl);
+
+              if (await canLaunch(url.toString())) {
+                await launch(url.toString());
+              } else {
+                await launch(url.toString());
+              }
+            },
+            child: const Text('Update Now'),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,57 +149,57 @@ class HomePage extends StatelessWidget {
                   ],
                 ),
               ),
-              const SliverToBoxAdapter(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 15),
-                    HomePageText(text: 'Product'),
-                    SizedBox(height: 10),
-                  ],
-                ),
-              ),
-              SliverGrid(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  childAspectRatio: 1,
-                ),
-                delegate: SliverChildListDelegate([
-                  const HomePageCard(
-                    image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
-                    text: 'BMI Calculetor',
-                    location: HomePage(),
-                  ),
-                  const HomePageCard(
-                    image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
-                    text: 'BMR Calculeror',
-                  ),
-                  const HomePageCard(
-                    image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
-                    text: 'Age Calculetor',
-                  ),
-                  const HomePageCard(
-                    image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
-                    text: 'Tip Calculetor',
-                  ),
-                  const HomePageCard(
-                    image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
-                    text: 'Profit Calculetor',
-                  ),
-                  const HomePageCard(
-                    image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
-                    text: 'Profit Calculetor',
-                  ),
-                  const HomePageCard(
-                    image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
-                    text: 'Profit Calculetor',
-                  ),
-                  const HomePageCard(
-                    image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
-                    text: 'Profit Calculetor',
-                  ),
-                ]),
-              ),
+              // const SliverToBoxAdapter(
+              //   child: Column(
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     children: [
+              //       SizedBox(height: 15),
+              //       HomePageText(text: 'Product'),
+              //       SizedBox(height: 10),
+              //     ],
+              //   ),
+              // ),
+              // SliverGrid(
+              //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              //     crossAxisCount: 3,
+              //     childAspectRatio: 1,
+              //   ),
+              //   delegate: SliverChildListDelegate([
+              //     const HomePageCard(
+              //       image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
+              //       text: 'BMI Calculetor',
+              //       location: HomePage(),
+              //     ),
+              //     const HomePageCard(
+              //       image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
+              //       text: 'BMR Calculeror',
+              //     ),
+              //     const HomePageCard(
+              //       image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
+              //       text: 'Age Calculetor',
+              //     ),
+              //     const HomePageCard(
+              //       image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
+              //       text: 'Tip Calculetor',
+              //     ),
+              //     const HomePageCard(
+              //       image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
+              //       text: 'Profit Calculetor',
+              //     ),
+              //     const HomePageCard(
+              //       image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
+              //       text: 'Profit Calculetor',
+              //     ),
+              //     const HomePageCard(
+              //       image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
+              //       text: 'Profit Calculetor',
+              //     ),
+              //     const HomePageCard(
+              //       image: 'https://i.postimg.cc/QMWYLmrh/bmi.png',
+              //       text: 'Profit Calculetor',
+              //     ),
+              //   ]),
+              // ),
             ],
           ),
         ),
